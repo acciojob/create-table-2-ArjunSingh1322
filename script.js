4677f32c3886aa1ebfaf985fd1b163197128f58f
ArjunSingh1322 <as3388776@gmail.com>
2025-03-06 11:50:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 function createTable() {
-    //Write your code here
-  
+    let rows = prompt("Input number of rows:");  
+    let cols = prompt("Input number of columns"); 
+
+    
+    rows = parseInt(rows);
+    cols = parseInt(cols);
+
+    
+    if (isNaN(rows) || isNaN(cols) || rows <= 0 || cols <= 0) {
+        alert("Please enter valid numbers for rows and columns!");
+        return;
+    }
+
+    let container = document.getElementById("table-container");
+    container.innerHTML = ""; 
+
+    let table = document.createElement("table");
+    table.border = "1"; 
+	table.id = "myTable"
+
+    for (let i = 0; i < rows; i++) {
+        let tr = document.createElement("tr");
+        for (let j = 0; j < cols; j++) {
+            let td = document.createElement("td");
+            td.innerText = `row${i}-column${j}`; 
+            td.style.padding = "10px"; 
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+
+    container.appendChild(table); 
 }
